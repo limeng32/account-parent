8f4c1c1579abc2015c7759f3336ee53ebd7cc30e
limeng32 <limeng32@live.cn>
2015-11-18 22:13:09 +0800

    Develop signOut method.

--- account-web/src/main/java/limeng32/mirage/account/web/AccountController.java
+++ account-web/src/main/java/limeng32/mirage/account/web/AccountController.java
@@ -1,12 +1,16 @@
 package limeng32.mirage.account.web;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import limeng32.mirage.account.persist.AccountPersistService;
 import limeng32.mirage.account.service.AccountService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,4 +31,12 @@ public class AccountController {
 		return "index";
 	}
 
+	@RequestMapping(method = RequestMethod.POST, value = "/signOut")
+	public String checkExist(HttpServletRequest request,
+			HttpServletResponse response, ModelMap mm) throws IOException {
+		request.getSession().removeAttribute("accountToken");
+		request.getSession().removeAttribute("authToken");
+		mm.addAttribute("_content", true);
+		return AccountSignUpController.UNIQUE_VIEW_NAME;
+	}
 }
